f744053d18a08c99db2cd6baee6aec42d9cf7e24
Martin <ml@example.com>
2022-07-09 19:57:51 +0200

    move logic to callable function

--- remove-orphans.py
+++ remove-orphans.py
@@ -122,6 +122,7 @@ import errno
 import re
 import shutil
 import sys
+import collections
 
 
 def stderr(line):
@@ -130,16 +131,12 @@ def stderr(line):
 
 
 def delete(files, backup_folder=None, verbose=True):
-    if len(files) == 0:
-        return
-    
-    if backup_folder:
-        for filename in files:
+    for filename in files:
+        if backup_folder:
             if verbose:
                 print(f"Moving {filename} to {backup_folder}.")
             shutil.move(filename, os.path.join(backup_folder))
-    else:
-        for filename in files:
+        else:
             if verbose:
                 print(f"Deleting {filename}.")
             os.remove(filename)
@@ -152,7 +149,62 @@ def listFullNamesFilesInTree(top):
             full_names.append(os.path.join(root, name))
     return full_names
 
-  
+
+def moveOrphans(rawFolder, jpegFolder, backupDir=None, verbose=True):
+    raw_images = []
+
+    # Collect all names from all subdirectories.
+    raw_files = listFullNamesFilesInTree(rawFolder)
+
+    # there could be JPEGs even in the raw directories
+    jpg_files = raw_files
+    for dirName in jpegFolder:
+        jpg_files += listFullNamesFilesInTree(dirName)
+
+    # list of bare names (without path) to be able to identify duplicate items
+    raw_images_bare_names = []
+    # sort files into raw and jpeg files
+    for filename in raw_files:
+        # The file name of raw image ends with .RW2 for Lumix and .NEF for Nikon
+        if re.match(r'(.*)\.(rw|nef|raf)$', filename, re.IGNORECASE):
+            basename = os.path.splitext(os.path.basename(filename))[0]
+            if basename in raw_images_bare_names:
+                stderr(f"{filename!r} is a duplicate raw file")
+            else:
+                raw_images_bare_names.append(basename)
+                raw_images.append(filename)
+
+    # list of bare names (without path) to be able to identify duplicate items
+    jpeg_images_bare_names = []
+    # Check names only, not directories.
+    for filename in jpg_files:
+        if re.match(r'(.*)\.jp(e)?g$', filename, re.IGNORECASE):
+            basename = os.path.splitext(os.path.basename(filename))[0]
+            if basename in jpeg_images_bare_names:
+                stderr(f"{filename!r} is a duplicate jpeg file")
+            else:
+                jpeg_images_bare_names.append(basename)
+
+    # Check if each raw has a corresponding jpeg
+    orphans = []
+    for raw_image in raw_images:
+        if os.path.splitext(os.path.basename(raw_image))[0] not in jpeg_images_bare_names:
+            orphans.append(raw_image)
+
+    if backupDir:
+        try:
+            os.mkdir(backupDir)
+        except OSError as e:
+            if not e.errno == errno.EEXIST:
+                raise
+
+    delete(orphans, backup_folder=backupDir, verbose=verbose)
+    matched_jpegs = len(raw_images) - len(orphans)
+
+    Result = collections.namedtuple("Result", ["rawTotal", "rawMoved", "jpegTotal", "jpegUnmatched"])
+    return Result(len(raw_images), len(orphans), len(jpeg_images_bare_names), matched_jpegs)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Cleanup of leftover raw image files (*.RW2 *.NEF *.RAF).',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -167,69 +219,12 @@ if __name__ == '__main__':
     parser.add_argument('jpg_folders', metavar='JPG_folders', nargs='*',
                         help='More folder(s) to check for JPGs. ')
     args = parser.parse_args()
-
     verbose = not args.quiet
-    raw_images, jpeg_images_bare_names, raw_images_bare_names = [], [], []
-
-    # Collect all names from all subdirectories.
-    raw_files = listFullNamesFilesInTree(args.raw_folder)
-        
-    jpg_files = raw_files
-    if len(args.jpg_folders) != 0:
-        for dirName in args.jpg_folders:
-            jpg_files += listFullNamesFilesInTree(dirName)
-
-    # sort files into raw and jpeg files
-    for filename in raw_files:
-        # The file name of raw image ends with .RW2 for Lumix and .NEF for Nikon
-        if re.match(r'(.*)\.(rw|nef|raf)$', filename, re.IGNORECASE):
-            basename = os.path.splitext(os.path.basename(filename))[0]
-            if basename in raw_images_bare_names:
-                stderr("'{}' is a duplicate file".format(filename))
-            else:
-                raw_images_bare_names.append(basename)
-                raw_images.append(filename)
-    
-    # Check names only, not directories.        
-    for filename in jpg_files:          
-        if re.match(r'(.*)\.jp(e)?g$', filename, re.IGNORECASE):
-            basename = os.path.splitext(os.path.basename(filename))[0]
-            if basename in jpeg_images_bare_names:
-                stderr("'{}' is a duplicate file".format(filename))
-            else:
-                jpeg_images_bare_names.append(basename)
-            
-    # Check if there is a jpeg for each raw image
-    orphans = []
-    for raw_image in raw_images:
-        if os.path.splitext(os.path.basename(raw_image))[0] not in jpeg_images_bare_names:
-            orphans.append(raw_image)
-    
-    # Exit if no work to do.        
-    if len(raw_images) + len(jpeg_images_bare_names) == 0:
-        if verbose:
-            stderr(f"No images found. No use checking {args.raw_folder!r} for orphaned RAW images")
-        sys.exit(2)
-    elif len(raw_images) == 0:
-        if verbose:
-            print(f"No RAW images found, but {len(jpeg_images_bare_names)} JPEGs. Won't do anything now.")
-        sys.exit(0)
-    elif len(orphans) == 0:
-        if verbose:
-            print(f"{len(raw_images)} RAW images found, and {len(jpeg_images_bare_names)} JPEGs but no orphans. "
-                  f"Won't do anything now.")
-        sys.exit(0)
-        
     backup_folder = None if args.no_backup else args.backup_folder
-    if backup_folder:
-        try:
-            os.mkdir(backup_folder)
-        except OSError as e:
-            if not e.errno == errno.EEXIST:
-                raise
-                
-    delete(orphans, backup_folder=backup_folder, verbose=verbose)
-    matched_jpegs = len(raw_images)-len(orphans)
-    print(f"matched JPEGs:      {matched_jpegs:4d}\n"
-          f"unmatched JPEGs:    {len(jpeg_images_bare_names)-matched_jpegs:4d}\n"
-          f"orphaned raw moved: {len(orphans):4d}")
+
+    moveResult = moveOrphans(args.raw_folder, args.jpg_folders, backup_folder, verbose)
+
+    print(f"total RAWs:         {moveResult.rawTotal:4d}\n"
+          f"matched JPEGs:      {(moveResult.jpegTotal - moveResult.jpegUnmatched):4d}\n"
+          f"unmatched JPEGs:    {moveResult.jpegUnmatched:4d}\n"
+          f"orphaned RAW moved: {moveResult.rawMoved:4d}")
